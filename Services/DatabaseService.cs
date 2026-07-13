63c0db242f23a2aa47fd66e68c3281d47c6974ad
mahmutmahmutgumus38 <mahmutmahmutgumus@gmail.com>
2026-07-13 15:31:19 +0300

    Create DatabaseService.cs

---
+++ Services/DatabaseService.cs
@@ -0,0 +1,19 @@
+using Microsoft.Data.SqlClient;
+
+namespace DataKapatmaMerkezi.Services;
+
+public class DatabaseService
+{
+    private readonly IConfiguration _configuration;
+
+    public DatabaseService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public SqlConnection CreateConnection()
+    {
+        return new SqlConnection(
+            _configuration.GetConnectionString("SqlConnection"));
+    }
+}
